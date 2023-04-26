0baa47c194a82a22cce2c44011f82541e880eb9d
HoaiTrang0305 <2014496@dlu.edu.vn>
2023-04-26 22:29:39 +0700

    Hoàn thành các API của người dùng và bài viết

--- src/SQL/QLDSVH.sql
+++ src/SQL/QLDSVH.sql
@@ -15,7 +15,7 @@ CREATE TABLE `BaiViet` (
   `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `NguoiDung` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
@@ -27,7 +27,7 @@ CREATE TABLE `NguoiDung` (
   `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `BaiViet` (`id`, `TieuDe`, `NoiDung`, `HinhAnh`, `create_at`, `update_at`) VALUES
 (1, ' Nhã nhạc cung đình Huế', 'Nhã nhạc là âm nhạc cung đình thời phong kiến, được trình diễn trong các dịp triều hội, tế lễ hoặc các sự kiện trọng đại (lễ đăng quang của nhà vua, tiếp đón sứ thần…).', NULL, '2023-04-25 08:34:52', '2023-04-25 08:34:52');
@@ -37,10 +37,13 @@ INSERT INTO `BaiViet` (`id`, `TieuDe`, `NoiDung`, `HinhAnh`, `create_at`, `updat
 (3, ' Dân ca quan họ Bắc Ninh', ' Quan họ là những làn điệu dân ca của vùng Đồng bằng Bắc Bộ, tập trung chủ yếu ở vùng Kinh Bắc (Bắc Ninh và Bắc Giang). Đây là hình thức hát đối đáp giữa nam và nữ để biểu lộ tâm tình, ca tụng tình yêu thông qua những câu ca mộc mạc, đằm thắm. Hầu hết các bài Quan họ đều theo thể lục bát hay lục bát biến thể.', NULL, '2023-04-25 08:35:52', '2023-04-25 08:35:52');
 INSERT INTO `BaiViet` (`id`, `TieuDe`, `NoiDung`, `HinhAnh`, `create_at`, `update_at`) VALUES
 (4, 'Ca trù', 'Ca trù (hay còn gọi là hát ả đào) có vị trí đặc biệt trong kho tàng âm nhạc truyền thống của Việt Nam, gắn liền với lễ hội, phong tục, tín ngưỡng, văn chương, tư tưởng và triết lý sống của người Việt. Loại hình nghệ thuật này rất phổ biến trong đời sống sinh hoạt văn hóa ở Việt Nam từ đầu thế kỷ XX trở về trước.', NULL, '2023-04-25 08:38:53', '2023-04-25 08:38:53'),
-(5, 'Hội Gióng ở đền Phù Đổng và đền Sóc', ' Hội Gióng ở đền Phù Đổng và đền Sóc (Hà Nội) gắn với truyền thuyết về một cậu bé được mẹ sinh ra một cách kỳ lạ ở làng Phù Đổng.\r\n	Hội Gióng ở đền Phù Đổng (xã Phù Đổng, huyện Gia Lâm - nơi Thánh Gióng sinh ra) diễn ra từ ngày 7/9 tháng tư Âm lịch. Hội Gióng ở đền Sóc (xã Phù Linh, huyện Sóc Sơn nơi Thánh hóa, cưỡi ngựa về trời) diễn ra từ ngày 6/8 tháng Giêng Âm lịch.', NULL, '2023-04-25 08:38:53', '2023-04-25 08:38:53');
+(5, 'Hội Gióng ở đền Phù Đổng và đền Sóc', ' Hội Gióng ở đền Phù Đổng và đền Sóc (Hà Nội) gắn với truyền thuyết về một cậu bé được mẹ sinh ra một cách kỳ lạ ở làng Phù Đổng.\r\n	Hội Gióng ở đền Phù Đổng (xã Phù Đổng, huyện Gia Lâm - nơi Thánh Gióng sinh ra) diễn ra từ ngày 7/9 tháng tư Âm lịch. Hội Gióng ở đền Sóc (xã Phù Linh, huyện Sóc Sơn nơi Thánh hóa, cưỡi ngựa về trời) diễn ra từ ngày 6/8 tháng Giêng Âm lịch.', NULL, '2023-04-25 08:38:53', '2023-04-25 08:38:53'),
+(7, 'TieuDe update', 'NoiDungMoi1', NULL, '2023-04-26 15:18:40', '2023-04-26 15:21:50');
 
 INSERT INTO `NguoiDung` (`id`, `Ten`, `DiaChi`, `Email`, `MatKhau`, `QuyenTruyCap`, `create_at`, `update_at`) VALUES
 (1, 'hoaiTrang', NULL, '2014496@dlu.edu.vn', '1234567', 1, '2023-04-25 08:33:45', '2023-04-25 08:33:45');
+INSERT INTO `NguoiDung` (`id`, `Ten`, `DiaChi`, `Email`, `MatKhau`, `QuyenTruyCap`, `create_at`, `update_at`) VALUES
+(3, 'tenmoi1', NULL, '2014422@dlu.edu.vn', '1234567', NULL, '2023-04-26 14:40:20', '2023-04-26 14:40:20');
 
 
 
--- src/main.go
+++ src/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,80 +11,418 @@ import (
 	"gorm.io/gorm"
 )
 
-type TodoItem struct {
-	Id           int        `json:"id"`
-	Ten          string     `json:"ten"`
-	DiaChi       string     `json:"diachi"`
-	Email        string     `json:"email"`
-	MatKhau      string     `json:"matkhau"`
-	QuyenTruyCap bool       `json:"quyentruycap"`
-	CreateAt     *time.Time `json:"create_at"`
-	UpdateAt     *time.Time `json:"update_at,omitempty"`
+//////////////Người dùng
+
+type NguoiDung struct {
+	Id           int        `json:"id" gorm:"column:Id;"`
+	Ten          string     `json:"ten" gorm:"column:Ten;"`
+	DiaChi       string     `json:"diachi" gorm:"column:DiaChi;"`
+	Email        string     `json:"email" gorm:"column:Email;"`
+	MatKhau      string     `json:"matkhau" gorm:"column:MatKhau;"`
+	QuyenTruyCap bool       `json:"quyentruycap" gorm:"column:QuyenTruyCap;"`
+	CreateAt     *time.Time `json:"create_at" gorm:"column:create_at;"`
+	UpdateAt     *time.Time `json:"update_at,omitempty" gorm:"column:update_at;"`
 }
 
-type TodoItemCreation struct {
-	Ten          string     `json:"ten"`
-	Email        string     `json:"email"`
-	MatKhau      string     `json:"matkhau"`
-	QuyenTruyCap bool       `json:"quyentruycap"`
+func (NguoiDung) TableName() string {
+	return "NguoiDung"
 }
 
+type NguoiDungCreation struct {
+	Id      int    `json:"-"  gorm:"column:Id;"`
+	Ten     string `json:"ten" gorm:"column:Ten;"`
+	Email   string `json:"email" gorm:"column:Email;"`
+	MatKhau string `json:"matkhau" gorm:"column:MatKhau;"`
+	//QuyenTruyCap bool   `json:"quyentruycap" gorm:"column:QuyenTruyCap;"`
+}
+
+func (NguoiDungCreation) TableName() string {
+	return NguoiDung{}.TableName()
+}
+
+type NguoiDungUpdate struct {
+	Ten     *string `json:"ten" gorm:"column:Ten;"`
+	Email   string  `json:"email" gorm:"column:Email;"`
+	DiaChi  *string `json:"diachi" gorm:"column:DiaChi;"`
+	MatKhau string  `json:"matkhau" gorm:"column:MatKhau;"`
+}
+
+func (NguoiDungUpdate) TableName() string {
+	return NguoiDung{}.TableName()
+}
+
+type Paging struct {
+	Page  int   `json:"page" form:"page"`
+	Limit int   `json:"limit" form:"limit"`
+	Total int64 `json:"total" form:"-"`
+}
+
+func (p *Paging) Process() {
+	if p.Page <= 0 {
+		p.Page = 1
+	}
+
+	if p.Limit <= 0 || p.Limit >= 100 {
+		p.Limit = 10
+	}
+}
 func main() {
-	dsn := os.Getenv("DB_CONN_STR")
+	dsn := "root:my-secret-pw@tcp(127.0.0.1:3307)/QLDSVH?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(db)
-
-	now := time.Now().UTC()
-
-	item := TodoItem{
-		Id:           1,
-		Ten:          "hoaiTrang",
-		DiaChi:       "hhhh",
-		Email:        "2014496@dlu.edu.vn",
-		MatKhau:      "1234567",
-		QuyenTruyCap: true,
-		CreateAt:     &now,
-		UpdateAt:     &now,
-	}
-
 	r := gin.Default()
 
-	//CRUD: Create,Read,update,Delete
-	//POST /v1/items(create a new item)
-	//GET /v1/items(list items) /v1/items?page=1
-	//GET /v1/items/:id(get item detail by id)
-	//(PUT||PATCH) /v1/items/:id(update item by id)
-	//DELETE /v1/items/:id(delete item by id)
-
-	v1:=r.Group(relativePath"/v1")
+	v1 := r.Group("/v1")
 	{
-		items:=v1.Group(relativePath:"/items")
+		NguoiDung := v1.Group("/nguoidung")
 		{
-			items.POST(relativePath:"", CreateItem())
-			items.GET(relativePath:"")
-			items.GET(relativePath:"/:id")
-			items.PATCH(relativePath:"/:id")
-			items.DELETE(relativePath:"/:id")
+			NguoiDung.POST("", CreateNguoiDung(db))
+			NguoiDung.GET("", ListNguoiDung(db))
+			NguoiDung.GET("/:id", GetNguoiDung(db))
+			NguoiDung.PATCH("/:id", UpdateNguoiDung(db))
+			NguoiDung.DELETE("/:id", DeleteNguoiDung(db))
+
+		}
+
+		BaiViet := v1.Group("/baiviet")
+		{
+			BaiViet.POST("", CreateBaiViet(db))
+			BaiViet.GET("", ListBaiViet(db))
+			BaiViet.GET("/:id", GetBaiViet(db))
+			BaiViet.PATCH("/:id", UpdateBaiViet(db))
+			BaiViet.DELETE("/:id", DeleteBaiViet(db))
 
 		}
 	}
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": item,
+			"message": "pong",
 		})
 	})
 
 	r.Run(":3000")
 }
 
-func CreateItem() func(*gin.Context){
+func CreateNguoiDung(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data NguoiDungCreation
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data.Id,
+		})
+	}
+}
+
+func GetNguoiDung(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data NguoiDung
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id=?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
+}
+
+func UpdateNguoiDung(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data NguoiDungUpdate
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id=?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func DeleteNguoiDung(db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Table(NguoiDung{}.TableName()).Where("id=?", id).Delete(nil).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func ListNguoiDung(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var paging Paging
+
+		if err := c.ShouldBind(&paging); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		paging.Process()
+
+		var result []NguoiDung
+
+		if err := db.Table(NguoiDung{}.TableName()).Count(&paging.Total).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Order("id desc").
+			Offset((paging.Page - 1) * paging.Limit).
+			Limit(paging.Limit).
+			Find(&result).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data":   result,
+			"paging": paging,
+		})
+	}
+}
+
+/////////////////Bài viết
+
+type BaiViet struct {
+	Id       int        `json:"id" gorm:"column:Id;"`
+	TieuDe   string     `json:"tieude" gorm:"column:TieuDe;"`
+	NoiDung  string     `json:"noidung" gorm:"column:NoiDung;"`
+	HinhAnh  string     `json:"hinhanh" gorm:"column:HinhAnh;"`
+	CreateAt *time.Time `json:"create_at" gorm:"column:create_at;"`
+	UpdateAt *time.Time `json:"update_at,omitempty" gorm:"column:update_at;"`
+}
+
+func (BaiViet) TableName() string {
+	return "BaiViet"
+}
+
+type BaiVietCreation struct {
+	Id      int    `json:"-"  gorm:"column:Id;"`
+	TieuDe  string `json:"tieude" gorm:"column:TieuDe;"`
+	NoiDung string `json:"noidung" gorm:"column:NoiDung;"`
+	//HinhAnh        string     `json:"hinhanh" gorm:"column:HinhAnh;"`
+}
+
+func (BaiVietCreation) TableName() string {
+	return BaiViet{}.TableName()
+}
+
+type BaiVietUpdate struct {
+	TieuDe  string `json:"tieude" gorm:"column:TieuDe;"`
+	NoiDung string `json:"noidung" gorm:"column:NoiDung;"`
+}
+
+func (BaiVietUpdate) TableName() string {
+	return BaiViet{}.TableName()
+}
+
+func CreateBaiViet(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data BaiVietCreation
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data.Id,
+		})
+	}
+}
+
+func GetBaiViet(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data BaiViet
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id=?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
+}
+
+func UpdateBaiViet(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data BaiVietUpdate
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id=?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func DeleteBaiViet(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Table(BaiViet{}.TableName()).Where("id=?", id).Delete(nil).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func ListBaiViet(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var paging Paging
+
+		if err := c.ShouldBind(&paging); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		paging.Process()
+
+		var result []BaiViet
+
+		if err := db.Table(BaiViet{}.TableName()).Count(&paging.Total).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Order("id desc").
+			Offset((paging.Page - 1) * paging.Limit).
+			Limit(paging.Limit).
+			Find(&result).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data":   result,
+			"paging": paging,
+		})
 	}
 }
