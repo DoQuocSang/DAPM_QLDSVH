2a5888110fad9006561c2ed9ade4373424b8260a
DoQuocSang <2014483@dlu.edu.vn>
2023-06-06 19:11:36 +0700

    Thêm giao diện 3 trang người dùng, đơn vị quản lý, địa điểm

--- src/web-frontend/Nhom10_QLDSVH/src/App.js
+++ src/web-frontend/Nhom10_QLDSVH/src/App.js
@@ -14,6 +14,14 @@ import AddOrUpdateHeritage from "pages/admin/heritage/AddOrUpdateHeritage"
 import AllHeritageType from "pages/admin/heritage-type/AllHeritageType"
 import AddOrUpdateHeritageType from "pages/admin/heritage-type/AddOrUpdateHeritageType"
 
+import AllLocation from "pages/admin/location/AllLocation"
+import AddOrUpdateLocation from "pages/admin/location/AddOrUpdateLocation"
+
+import AllManagementUnit from "pages/admin/management-unit/AllManagementUnit"
+import AddOrUpdateManagementUnit from "pages/admin/management-unit/AddOrUpdateManagementUnit"
+
+import AllUser from "pages/admin/user/AllUser"
+import AddOrUpdateUser from "pages/admin/user/AddOrUpdateUser"
 
 import AdminLogin from "pages/admin/login/Login"
 
@@ -49,6 +57,18 @@ function App() {
           <Route path="/admin/dashboard/all-heritage-type" element={<AllHeritageType />} />
           <Route path="/admin/dashboard/add-heritage-type" element={<AddOrUpdateHeritageType type="add" />} />
           <Route path="/admin/dashboard/update-heritage-type/:id" element={<AddOrUpdateHeritageType type="update" />} />
+       
+          <Route path="/admin/dashboard/all-location" element={<AllLocation />} />
+          <Route path="/admin/dashboard/add-location" element={<AddOrUpdateLocation type="add" />} />
+          <Route path="/admin/dashboard/update-location/:id" element={<AddOrUpdateLocation type="update" />} />
+
+          <Route path="/admin/dashboard/all-management-unit" element={<AllManagementUnit />} />
+          <Route path="/admin/dashboard/add-management-unit" element={<AddOrUpdateManagementUnit type="add" />} />
+          <Route path="/admin/dashboard/update-management-unit/:id" element={<AddOrUpdateManagementUnit type="update" />} />
+        
+          <Route path="/admin/dashboard/all-user" element={<AllUser />} />
+          <Route path="/admin/dashboard/add-user" element={<AddOrUpdateUser type="add" />} />
+          <Route path="/admin/dashboard/update-user/:id" element={<AddOrUpdateUser type="update" />} />
         </Route>
 
         {/* <Route path="*" element={<NotFound404 />} /> */}
